fe632bf2d66f5f923535497d0e6f6682f5a50046
Javillcam6 <javillcam.6@gmail.com>
2023-08-26 11:42:13 -0700

    demo

---
+++ calculos.js
@@ -0,0 +1,4 @@
+const suma = (a, b) => console.log(`La suma es:`, a + b)
+const multiplicacion = (a, b) => console.log(`La multiplicacion es igual a:`, a*b)
+
+module.exports = {multiplicacion, suma} 
---
+++ index.js
@@ -0,0 +1,5 @@
+const {multiplicacion, suma} = require ('./calculos.js')
+
+suma (3,7)
+multiplicacion (3,7)
+
